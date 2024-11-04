ea9a66697e4f696e27ae145029105fc432d26926
Eduardo Vázquez <jevo1900@gmail.com>
2024-11-04 12:17:40 +0100

    feat(lib): macro update

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default, Clone, PartialEq)]
 pub struct DiscordAccessToken {
     pub access_token: String,
     pub token_type: String,
     pub expires_in: i64,
 }
 
-#[derive(Serialize, Clone, Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize, Default, Clone, PartialEq)]
 pub struct DiscordUser {
     pub id: String,
     pub username: String,
@@ -15,7 +15,7 @@ pub struct DiscordUser {
     pub locale: Option<String>,
 }
 
-#[derive(Serialize, Clone, Deserialize, Debug)]
+#[derive(Debug, Serialize, Deserialize, Default, Clone, PartialEq)]
 pub struct DiscordUserComplete {
     pub id: String,
     pub username: String,
